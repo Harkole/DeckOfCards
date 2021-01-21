9b7df7d160f8d2bd85ea7390f69dd6ea55e7866f
Will Blackburn <harkole@gmail.com>
2021-01-21 22:21:10 +0000

    Updated props and wired up shuffle button

--- src/ui/deck-of-cards/src/containers/Deal.tsx
+++ src/ui/deck-of-cards/src/containers/Deal.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import "./Deal.css";
+import { ICard } from "./../App";
 
-export interface IDealProps {}
+export interface IDealProps {
+  cards: Array<ICard>;
+  getDeck: () => void;
+  shuffleDeck: () => void;
+};
 
 const Deal = (props: IDealProps) => {
+  console.log(props.cards.length);
+  
   return (
     <div className="Deal">
       <div className="Table">
         <header>
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" onClick={props.getDeck}>
             New Deck
           </Button>
-          <Button variant="contained" color="primary" disabled>
+          <Button variant="contained" color="primary" disabled={(props.cards.length === 52)} onClick={props.shuffleDeck}>
             Shuffle Deck
           </Button>
         </header>
